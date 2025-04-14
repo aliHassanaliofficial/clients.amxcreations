28ecbe2c2ddf09e2f15e767f599b832fa6b12483
aliHassanaliofficial <alihassanmagix2017@gmail.com>
2025-04-14 17:27:51 +0200

    modified:   src/app/auth/login.js

--- src/app/auth/login.js
+++ src/app/auth/login.js
@@ -11,16 +11,6 @@ function Authlogin() {
         return;
     }
     
-    // Simulate a login request
-    setTimeout(function () {
-        if (userIdValue === "admin" && passwordValue === "password") {
-        alert("Login successful!");
-        window.location.href = "./user.html"; // Redirect to home page
-        } else {
-        alert("Invalid credentials. Please try again.");
-        }
-    }, 1000);
-
     setTimeout(function () {
         if (userIdValue === "02548" && passwordValue === "01007001305" || userIdValue === "02547" && passwordValue === "01006990989") {
         alert("This user is not active yet. Please contact the administrator.");
