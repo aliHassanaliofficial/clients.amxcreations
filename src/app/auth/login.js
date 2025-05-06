b1d1b0c7948962f63c34032688d323ba3e3447b3
aliHassanaliofficial <alihassanmagix2017@gmail.com>
2025-05-06 16:54:33 +0300

    new file:   clients/02548/summaries/April/Apr-08-2025_May-05-2025_1683103542288463.csv new file:   clients/02548/summaries/April/E_Statment_02548.pdf new file:   clients/02548/summaries/April/index.html modified:   index.html modified:   src/app/auth/login.js

--- src/app/auth/login.js
+++ src/app/auth/login.js
@@ -5,15 +5,19 @@ var loginButton = document.getElementById("loginButton");
 function Authlogin() {
     var userIdValue = userId.value;
     var passwordValue = password.value;
-    
+
     if (userIdValue === "" || passwordValue === "") {
         alert("Please fill in all fields.");
         return;
     }
-    
+
     setTimeout(function () {
-        if (userIdValue === "02548" && passwordValue === "01007001305" || userIdValue === "02547" && passwordValue === "01006990989") {
-        alert("This user is not active yet. Please contact the administrator.");
+        if (userIdValue === "02547" && passwordValue === "01006990989") {
+            alert("This user is not active yet. Please contact the administrator.");
+        }
+
+        else if (userIdValue === "02548" && passwordValue === "01007001305") {
+            window.location.href = "home.html";
         }
     }, 1000);
-    }
+}
